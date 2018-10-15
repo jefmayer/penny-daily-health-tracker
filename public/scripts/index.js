7ecaee658b7e4d7c32284ab6bf48cda5c30383f9
Jef Mayer <modeseventyeight@gmail.com>
2018-10-15 08:41:44 -0500

    Adding in edit buttons, functionaliy

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -11,7 +11,16 @@ var app = new Vue({
 			pain: '5',
 			stress: '5',
 			notes: ''
-		}
+		},
+		defaults: {
+			date: '',
+			mobility: '5',
+			activity: '5',
+			appetite: '5',
+			pain: '5',
+			stress: '5',
+			notes: ''
+		},
 	},	
 	methods: {
 		getData: function() {
@@ -24,7 +33,7 @@ var app = new Vue({
         if (request.status >= 200 && request.status < 400) {
 					that.requesting = false;
 					that.datapoints = JSON.parse(request.responseText);
-					console.log(that.data);
+					console.log(that.datapoints);
         } else {
         	console.log(request.responseText);
         	console.warn('index.js, getRecords : error');
@@ -61,11 +70,10 @@ var app = new Vue({
 			}));
 		},
 		getTodaysFormattedDate: function() {
-			var d = new Date(),
-					s = '';
-			
-			s = d.getUTCFullYear() + '-' + this.addLeadingZero(d.getMonth() + 1) + '-' + this.addLeadingZero(d.getDate())
-			return s;
+			var d = new Date();
+			return d.getUTCFullYear() + '-' +
+			this.addLeadingZero(d.getMonth() + 1) + '-' +
+			this.addLeadingZero(d.getDate())
 		},
 		addLeadingZero: function(n) {
 			if (n.toString().length === 1) {
@@ -73,18 +81,29 @@ var app = new Vue({
 			}
 			return n;
 		},
-		reset: function() {
+		getDataByDate: function(date) {
+			for (var i = 0; i < this.datapoints.length; i++) {
+				if (this.datapoints[i].date === date) {
+					return this.datapoints[i];
+				}
+			}
+			return this.defaults;
+		},
+		resetFields: function() {
 			this.daily.date = this.getTodaysFormattedDate();
-			this.daily.mobility = 5;
-			this.daily.activity = 5;
-			this.daily.appetite = 5;
-			this.daily.pain = 5;
-			this.daily.stress = 5;
-			this.daily.notes = '';
+			this.populateFields(this.defaults);
+		},
+		populateFields: function(values) {
+			for (var e in this.daily) {
+				this.daily[e] = values[e];
+			}
 		},
 		refresh: function() {
-			this.reset();
+			this.resetFields();
 			this.getData();
+		},
+		edit: function(date) {
+			this.populateFields(this.getDataByDate(date));
 		}
 	},
 	mounted: function() {
